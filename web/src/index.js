2f1327d71fff211d76bf150c0529e77e36833159
Collin Potts <collinpotts@mit.edu>
2018-07-21 13:45:38 -0400

    Cleared files and directories, for new start

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import registerServiceWorker from './scripts/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
