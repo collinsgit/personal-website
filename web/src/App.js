cc730f352e8f59cd9cb1e6e3298d5b6c7999f2d3
Collin Potts <collinpotts@mit.edu>
2018-08-28 14:45:50 -0400

    Created welcome header

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles/App.css';
 import NavBar from './components/NavBar';
+import Welcome from './components/Welcome';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <NavBar />
+        <Welcome />
       </div>
     );
   }
---
+++ web/src/components/Welcome.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Welcome extends React.Component {
+  render() {
+    return (
+      <header className='welcome' style={{paddingTop: 'calc(6rem + 7px)', paddingBottom: '6rem'}}>
+        <div style={{display: 'flex', flexFlow: 'column'}}>
+          <img className='image-fluid my-2 mx-auto mb-5'
+            src={require('./../images/me_typing_icon.png')}
+            alt='Profile shot'
+            />
+          <h1 className='mx-auto light-text header-text mb-3'>COLLIN POTTS</h1>
+          <hr className='light-divider' />
+          <h2 className='mx-auto light-text mt-3' style={{fontSize: '1.3rem', fontWeight: '300'}}>
+            Software Engineer - Student - Mathematician
+          </h2>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Welcome;
