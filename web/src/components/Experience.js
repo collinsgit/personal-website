319895a48d6e8686c7f3204a2e88a11d34da28a3
Collin Potts <collinpotts@mit.edu>
2018-08-31 01:58:25 -0400

    Added experience buttons

--- web/src/components/Experience.js
+++ web/src/components/Experience.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ModalButton from './ModalButton';
 
 class Experience extends React.Component {
   render() {
@@ -6,10 +7,11 @@ class Experience extends React.Component {
       <div className='py-5 dark-background' style={{display: 'flex', flexFlow: 'column'}} id='experience'>
         <h1 className='light-text header-text mb-3'>experience</h1>
         <hr className='light-divider' />
-        <div className='mt-3 mx-auto' style={{maxWidth: '720px', paddingLeft: '15px', paddingRight: '15px'}}>
-          <p className='light-text'>
-            PORTFOLIO INFO
-          </p>
+        <div className='mt-3 mx-auto' style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly', maxWidth: '720px', paddingLeft: '15px', paddingRight: '15px'}}>
+          <ModalButton background='portfolio/wiki.png' text='WikipediaBase Project with MIT&#39;s Infolab' />
+          <ModalButton background='portfolio/jpmc.png' text='Technology Analyst Internship at JPMorgan Chase & Co.' />
+          <ModalButton background='portfolio/amp.png' text='Speech Recognition Project with MIT&#39;s Research Laboratory of Electronics' />
+          <ModalButton background='portfolio/telescope.png' text='Zero Robotics Project with MIT&#39;s Space System Laboratory' />
         </div>
       </div>
     );
---
+++ web/src/components/ModalButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ModalButton extends React.Component {
+  render() {
+    return (
+      <div className='modal-button mt-3 mx-2' style={{background: '#333a56', height: '221px', width: '315px'}}>
+        <div className='light-text header-text' style={{display: 'flex', flexFlow: 'column', height: '100%', justifyContent: 'space-around'}}>
+          <h3>{this.props.text}</h3>
+        </div>
+        <img className='image-fluid modal-image' src={require('../images/' + this.props.background)}
+             alt={this.props.background}
+             style={{height: '100%', width: '100%', position: 'relative', top: '-221px'}} />
+      </div>
+    );
+  }
+}
+
+export default ModalButton;
